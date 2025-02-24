41ebf0fd0b4b4275391e4f8485355caae83fc2fc
Nelani Maluka <malukanelani@gmail.com>
2025-02-24 17:09:03 +0200

    updated code structure

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
 import WorkIcon from "@mui/icons-material/Work";
 import SchoolIcon from "@mui/icons-material/School";
 import CustomizedTimeline from "./Timeline";
 import Education from "./Education";
-import { useState } from "react";
 
 import "./Experience.css";
 
 export default function Experience() {
   const [isView, setView] = useState(<CustomizedTimeline />);
   const [isSelected, setSelected] = useState("experience");
+  const [hoveredIcon, setHoveredIcon] = useState(null);
+
+  const handleMouseEnter = (icon) => {
+    setHoveredIcon(icon);
+  };
 
   const handleExperienceClick = () => {
     setSelected("experience");
@@ -22,12 +27,29 @@ export default function Experience() {
     setView(<Education />);
   };
 
+  const icons = [
+    { name: "React js", src: "/Images/Icons/react.png" },
+    { name: "Redux", src: "/Images/Icons/Redux.png" },
+    { name: "Spring Boot", src: "/Images/Icons/spring-boot.png" },
+    { name: "Java", src: "/Images/Icons/java.png" },
+    { name: "Jquery", src: "/Images/Icons/jquery.png" },
+    { name: "JavaScript", src: "/Images/Icons/js.png" },
+    { name: "CSS", src: "/Images/Icons/css-3.png" },
+    { name: "Git", src: "/Images/Icons/Git.png" },
+    { name: "Junit", src: "/Images/Icons/Junit.png" },
+    { name: "Mysql", src: "/Images/Icons/mysql.png" },
+    { name: "MS Sql", src: "/Images/Icons/sql-server.png" },
+    { name: "Oracle DB", src: "/Images/Icons/oracle.png" },
+    { name: "Azure", src: "/Images/Icons/azure.png" },
+    { name: "Firebase", src: "/Images/Icons/firebase.png" },
+  ];
+
   return (
     <div id="Experience" className="xp-container">
       <div className="container">
         <div className="heading">
           <h1>Experience and Skills</h1>
-          <p>
+          <p className="sub-heading">
             Explore my skills and experience in software development. See how I
             build secure, scalable applications.
           </p>
@@ -51,64 +73,19 @@ export default function Experience() {
             <div className="timeline-wrapper">{isView}</div>
           </div>
           <div className="skills">
-            <span>
-              <img src="/Images/Icons/react.png" alt="React icon" />
-              React js
-            </span>
-            <span>
-              <img src="/Images/Icons/Redux.png" alt="React icon" />
-              Redux
-            </span>
-            <span>
-              <img src="/Images/Icons/spring-boot.png" alt="Spring Boot icon" />
-              Spring Boot
-            </span>
-            <span>
-              <img src="/Images/Icons/java.png" alt="Java icon" />
-              Java
-            </span>
-            <span>
-              <img src="/Images/Icons/jquery.png" alt="Jquery icon" />
-              Jquery
-            </span>
-            <span>
-              <img src="/Images/Icons/js.png" alt="JavaScript icon" />
-              JavaScript
-            </span>
-            <span>
-              <img src="/Images/Icons/css-3.png" alt="CSS icon" />
-              CSS
-            </span>
-
-            <span>
-              <img src="/Images/Icons/Git.png" alt="Git icon" />
-              Git
-            </span>
-
-            <span>
-              <img src="/Images/Icons/Junit.png" alt="Junit icon" />
-              Junit
-            </span>
-            <span>
-              <img src="/Images/Icons/mysql.png" alt="Mysql icon" />
-              Mysql
-            </span>
-            <span>
-              <img src="/Images/Icons/sql-server.png" alt="MS Sql icon" />
-              MS Sql
-            </span>
-            <span>
-              <img src="/Images/Icons/oracle.png" alt="Oracle DB icon" />
-              Oracle DB
-            </span>
-            <span>
-              <img src="/Images/Icons/azure.png" alt="Azure icon" />
-              Azure
-            </span>
-            <span>
-              <img src="/Images/Icons/firebase.png" alt="Firebase icon" />
-              Firebase
-            </span>
+            {icons.map((icon, index) => (
+              <span key={index}>
+                <img
+                  src={icon.src}
+                  alt={`${icon.name} icon`}
+                  className={`rotate ${
+                    hoveredIcon === icon.name ? "rotating" : ""
+                  }`}
+                  onMouseEnter={() => handleMouseEnter(icon.name)}
+                />
+                <span className="text">{icon.name}</span>
+              </span>
+            ))}
           </div>
         </div>
       </div>
