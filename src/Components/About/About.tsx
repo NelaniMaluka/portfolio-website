df23eb07f485d331b73472ef63de82d74364e870
Nelani Maluka <malukanelani@gmail.com>
2026-04-01 09:32:34 +0200

    chore: updated resume

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -8,161 +8,162 @@ import { useEffect, useRef, useState } from "react";
 
 import styles from "./About.module.css";
 
-export default function About() {   
-    const statsRef = useRef(null);
-    const [startAnimation, setStartAnimation] = useState(false);
+export default function About() {
+  const statsRef = useRef(null);
+  const [startAnimation, setStartAnimation] = useState(false);
 
-    const [counts, setCounts] = useState({
+  const [counts, setCounts] = useState({
     exp: 0,
     projects: 0,
     certs: 0,
-    });
+  });
 
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            ([entry]) => {
-            if (entry.isIntersecting) {
-                setStartAnimation(true);
-            }
-            },
-            { threshold: 0.5 }
-        );
-
-        if (statsRef.current) {
-            observer.observe(statsRef.current);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setStartAnimation(true);
         }
+      },
+      { threshold: 0.5 },
+    );
 
-        return () => observer.disconnect();
-    }, []);
+    if (statsRef.current) {
+      observer.observe(statsRef.current);
+    }
 
-    useEffect(() => {
-        if (!startAnimation) return;
+    return () => observer.disconnect();
+  }, []);
 
-        const duration = 1500; // animation time (ms)
+  useEffect(() => {
+    if (!startAnimation) return;
 
-        const animate = (start:number, end: number, key: keyof typeof counts) => {
-            let startTime: number | null = null;
+    const duration = 1500; // animation time (ms)
 
-            const step = (timestamp: number) => {
-            if (!startTime) startTime = timestamp;
-            const progress = timestamp - startTime;
+    const animate = (start: number, end: number, key: keyof typeof counts) => {
+      let startTime: number | null = null;
 
-            const value = Math.min(
-                Math.floor((progress / duration) * end),
-                end
-            );
+      const step = (timestamp: number) => {
+        if (!startTime) startTime = timestamp;
+        const progress = timestamp - startTime;
 
-            setCounts((prev) => ({ ...prev, [key]: value }));
+        const value = Math.min(Math.floor((progress / duration) * end), end);
 
-            if (progress < duration) {
-                requestAnimationFrame(step);
-            }
-            };
+        setCounts((prev) => ({ ...prev, [key]: value }));
 
-            requestAnimationFrame(step);
-        };
-
-        animate(0, 1, "exp");
-        animate(0, 7, "projects");
-        animate(0, 4, "certs");
-
-    }, [startAnimation]);
-
-    const educationData = [
-        {
-        year: "2023 - 2025",
-        name: "National Certificate in Information Technology: Database Development",
-        institution: "CTU Training Solutions",
-        description: "A comprehensive program focused on designing and implementing robust software solutions, specializing in cloud architecture, Azure services, and integrated security practices.",
+        if (progress < duration) {
+          requestAnimationFrame(step);
         }
-    ];
+      };
 
-const ServicesData = [
-  {
-    img: <WebIcon className={styles.icon} sx={{ fontSize: 25 }} />,
-    name: "Frontend Development",
-    tools: "HTML, CSS, JavaScript, React, TypeScript",
-  },
-  {
-    img: <CodeIcon className={styles.icon} sx={{ fontSize: 25 }} />,
-    name: "Backend Development",
-    tools: "Java, Spring Boot, Microservices, System Design",
-  },
-  {
-    img: <StorageIcon className={styles.icon} sx={{ fontSize: 25 }} />,
-    name: "SQL Development",
-    tools: "MSSQL, MySQL, Relational Databases, Query Optimization",
-  },
-  {
-    img: <CloudIcon className={styles.icon} sx={{ fontSize: 25 }} />,
-    name: "Cloud Architecture",
-    tools: "Azure, Real-Time Data Processing, Cloud Security",
-  },
-  {
-  img: <ApiIcon className={styles.icon} sx={{ fontSize: 25 }} />,
-  name: "API Design",
-  tools: "REST APIs, JSON, Request Validation, Pagination"
-  },
-  {
-  img: <SettingsEthernetIcon className={styles.icon} sx={{ fontSize: 25 }} />,
-  name: "DevOps & CI/CD",
-  tools: "GitHub Actions, Build Pipelines, Automated Deployment"
-  }
-];
-    
-    return (
-<div id="About" className={styles.about}>
-    <div className="container"> 
+      requestAnimationFrame(step);
+    };
+
+    animate(0, 1, "exp");
+    animate(0, 7, "projects");
+    animate(0, 4, "certs");
+  }, [startAnimation]);
+
+  const educationData = [
+    {
+      year: "2023 - 2025",
+      name: "Diploma in Information Technology: Systems Development",
+      institution: "CTU Training Solutions",
+      description:
+        "A comprehensive program focused on designing and implementing robust software solutions, specializing in cloud architecture, Azure services, and integrated security practices.",
+    },
+    // {
+    // year: "2023 - 2025",
+    // name: "National Certificate in Information Technology: Database Development",
+    // institution: "CTU Training Solutions",
+    // description: "A comprehensive program focused on designing and implementing robust software solutions, specializing in cloud architecture, Azure services, and integrated security practices.",
+    // }
+  ];
+
+  const ServicesData = [
+    {
+      img: <WebIcon className={styles.icon} sx={{ fontSize: 25 }} />,
+      name: "Frontend Development",
+      tools: "HTML, CSS, JavaScript, React, TypeScript",
+    },
+    {
+      img: <CodeIcon className={styles.icon} sx={{ fontSize: 25 }} />,
+      name: "Backend Development",
+      tools: "Java, Spring Boot, Microservices, System Design",
+    },
+    {
+      img: <StorageIcon className={styles.icon} sx={{ fontSize: 25 }} />,
+      name: "SQL Development",
+      tools: "MSSQL, MySQL, Relational Databases, Query Optimization",
+    },
+    {
+      img: <CloudIcon className={styles.icon} sx={{ fontSize: 25 }} />,
+      name: "Cloud Architecture",
+      tools: "Azure, Real-Time Data Processing, Cloud Security",
+    },
+    {
+      img: <ApiIcon className={styles.icon} sx={{ fontSize: 25 }} />,
+      name: "API Design",
+      tools: "REST APIs, JSON, Request Validation, Pagination",
+    },
+    {
+      img: (
+        <SettingsEthernetIcon className={styles.icon} sx={{ fontSize: 25 }} />
+      ),
+      name: "DevOps & CI/CD",
+      tools: "GitHub Actions, Build Pipelines, Automated Deployment",
+    },
+  ];
+
+  return (
+    <div id="About" className={styles.about}>
+      <div className="container">
         <div className={styles.heading}>
-            <h4>About</h4>
-            <p>
-                A brief overview of my education, technical skills, and the technologies I use to
-                design and build modern web applications.
-            </p>
+          <h4>About</h4>
+          <p>
+            A brief overview of my education, technical skills, and the
+            technologies I use to design and build modern web applications.
+          </p>
         </div>
         <div className={styles.content}>
-            <div className={styles.column1}> 
-
-                <div className={styles.education}>
-                    {educationData.map((edu, index) => (
-                        <div key={index} className={styles.educationItem}>
-                            <p>{edu.year}</p>
-                            <h6>{edu.name}</h6>
-                            <p>{edu.institution}</p>
-                            <p>{edu.description}</p>
-                        </div>
-                    ))}
+          <div className={styles.column1}>
+            <div className={styles.education}>
+              {educationData.map((edu, index) => (
+                <div key={index} className={styles.educationItem}>
+                  <p>{edu.year}</p>
+                  <h6>{edu.name}</h6>
+                  <p>{edu.institution}</p>
+                  <p>{edu.description}</p>
                 </div>
-
-                <div className={styles.statsBox} ref={statsRef}>
-                    <div className={styles.stat}>
-                        <h5>{counts.exp}+</h5>
-                        <p>Years Exp</p>
-                        </div>
-                        <div className={styles.stat}>
-                        <h5>{counts.projects}+</h5>
-                        <p>Projects</p>
-                        </div>
-                        <div className={styles.stat}>
-                        <h5>{counts.certs}+</h5>
-                        <p>Certifications</p>
-                        </div>
-                </div>
+              ))}
             </div>
-            <div className={styles.column2}> 
-                {ServicesData.map((service, index) => (
-                    <div key={index} className={styles.serviceItem}>
-                        <div className={styles.img}>
-                            {service.img}
-                        </div>
-                        <h6>{service.name}</h6>
-                        <p>{service.tools}</p>
-                    </div>
-                ))}</div>
+
+            <div className={styles.statsBox} ref={statsRef}>
+              <div className={styles.stat}>
+                <h5>{counts.exp}+</h5>
+                <p>Years Exp</p>
+              </div>
+              <div className={styles.stat}>
+                <h5>{counts.projects}+</h5>
+                <p>Projects</p>
+              </div>
+              <div className={styles.stat}>
+                <h5>{counts.certs}+</h5>
+                <p>Certifications</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.column2}>
+            {ServicesData.map((service, index) => (
+              <div key={index} className={styles.serviceItem}>
+                <div className={styles.img}>{service.img}</div>
+                <h6>{service.name}</h6>
+                <p>{service.tools}</p>
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
     </div>
-
-</div>
-
-);
+  );
 }
